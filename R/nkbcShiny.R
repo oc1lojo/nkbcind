f83a83cbb0696ec45a9003f416a35b558f6e1fa1
John Lövrot <john.lovrot@sll.se>
2019-10-21 21:24:01 +0200

    Lägg till wrapper för rccShiny2 på INCA

---
+++ R/nkbcShiny.R
@@ -0,0 +1,20 @@
+nkbcShiny <- function(nkbcind, inca = TRUE, ...) {
+  if (inca) {
+    rccShiny2(
+      inca = inca,
+      incaScript = file.path(Sys.getenv("ScriptPath"), "Stockholm", "Brostcancer", "nkbc-rccShiny-dm.R"),
+      outcome = outcome(nkbcind),
+      outcomeTitle = outcome_title(nkbcind),
+      periodDateLevel = "quarter", # OBS Annorlunda jfr med publika rapporter
+      periodLabel = "Kvartal för diagnos",
+      periodDefaultStart = paste0(year(today()) - 1, "Q1"),
+      periodDefaultEnd = paste0(year(today()) - 1, "Q4"),
+      textBeforeSubtitle = textBeforeSubtitle(nkbcind),
+      description = description(nkbcind, year(today())),
+      varOther = varOther(nkbcind),
+      propWithinUnit = ifelse(!is.null(prop_within_unit(nkbcind)), prop_within_unit(nkbcind), "dagar"), # work-around, använd standardvärde
+      propWithinValue = ifelse(!is.null(prop_within_value(nkbcind)), prop_within_value(nkbcind), 30), # work-around, använd standardvärde
+      targetValues = target_values(nkbcind)
+    )
+  }
+}
