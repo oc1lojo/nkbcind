c0d459dec08f4f0a3cddd3de1460dcbff542f2af
John Lövrot <john.lovrot@sll.se>
2020-05-12 17:31:50 +0200

    Lägg till prototyp för nkbc52, täckningsgrad för inrapportering av nya direktrekonstruktionsformuläret

---
+++ R/nkbc-tackning-for-dir-52.R
@@ -0,0 +1,30 @@
+nkbc52 <- list(
+  code = "nkbc52",
+  lab = "Täckningsgrad för rapportering av direktrekonstruktion/onkoplastikkirurgi (PROTOTYP)",
+  pop = "opererade fall med onkoplastikkirurg och utan fjärrmetastaser vid diagnos",
+  filter_pop = function(x, ...) {
+    dplyr::filter(
+      x,
+
+      # TODO Lämpligt urval? Formuläret driftsattes i slutet på feb 2020.
+      lubridate::year(a_diag_dat) >= 2020,
+
+      # Onkoplastikkirurgi
+      op_kir_onkoplastik_Varde %in% 1,
+
+      # Ej fjärrmetastaser vid diagnos
+      !a_tnm_mklass_Varde %in% 10
+    )
+  },
+  mutate_outcome = function(x, ...) {
+    dplyr::mutate(x,
+      outcome = ifelse(!is.na(dir_inr_dat) | !is.na(dir_inr_enh), TRUE, FALSE)
+    )
+  },
+  target_values = c(70, 85),
+  sjhkod_var = "op_inr_sjhkod",
+  om_indikatorn = "Rapportering av direktrekonstruktion/onkoplastikkirurgi sker på ett eget formulär till kvalitetsregistret (driftsatt feb 2020), separat från operationsformuläret.",
+  vid_tolkning = NULL,
+  teknisk_beskrivning = NULL
+)
+class(nkbc52) <- "nkbcind"
